5f71c9b51952b0bb044bbf2a1b1270b7b3d998a9
Carlos Santiago <c.santiago.cruz@gmail.com>
2022-05-09 23:40:30 -0500

    Add first commit of the script

---
+++ git-restore-ui.sh
@@ -0,0 +1,14 @@
+STAGED_FILES=$(git --no-pager diff --name-only --cached --diff-filter=AM)
+if [[ $STAGED_FILES ]]; then
+    let COUNTER=0
+    LINE=$( git --no-pager diff --name-only --cached --diff-filter=AM | while read STAGED_FILE
+    do
+        let "COUNTER+=1" 
+        echo "\"$STAGED_FILE\" \"$COUNTER\" off"
+    done)
+    echo $LINE;
+    SELECTED_STAGED_FILES=$(echo $LINE | xargs dialog --stdout --checklist "files to restore:" 0 0 0)
+    echo $SELECTED_STAGED_FILES | xargs git restore --staged
+else
+    echo  "files to restore don´t exist"
+fi
